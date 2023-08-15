413da612b54ff9e10fef56f212c3e417bd403cdf
Ryan R <irishreid55@gmail.com>
2023-08-15 12:40:34 -0700

    css added

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-
-
-//  code for tooltips
+//   code for tooltips
 const tooltips = document.querySelectorAll('.tt');
 tooltips.forEach(t => {
   new bootstrap.Tooltip(t);
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  const tooltips = document.querySelectorAll(".tt");
+  tooltips.forEach((t) => {
+    new bootstrap.Tooltip(t);
+  });
+});
 
