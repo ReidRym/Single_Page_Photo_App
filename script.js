3c647d1b085503b1e738bbb56f9f11b72ad978c7
Ryan R <irishreid55@gmail.com>
2023-08-23 21:47:04 -0700

    added links to pix in side modal

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 //   code for tooltips
-const tooltips = document.querySelectorAll('.tt');
+const tooltips = document.querySelectorAll(".tt");
 tooltips.forEach(t => {
   new bootstrap.Tooltip(t);
 });
 
 document.addEventListener("DOMContentLoaded", function () {
   const tooltips = document.querySelectorAll(".tt");
-  tooltips.forEach((t) => {
+  tooltips.forEach(t => {
     new bootstrap.Tooltip(t);
   });
 });
 
+document.getElementById("superhero-link").onclick = function () {
+  document.getElementById("image").src = "./assets/DSC_0316.jpg"; //  image source
+  document.getElementById("image-modal").style.display = "block";
+};
+
+document.getElementById("session-scare").onclick = function () {
+  document.getElementById("image").src = "./assets/DSC_0343.jpg"; //  image source
+  document.getElementById("image-modal").style.display = "block";
+};
+document.getElementById("levelling-up").onclick = function () {
+  document.getElementById("image").src = "./assets/DSC_0343.jpg"; //  image source
+  document.getElementById("image-modal").style.display = "block";
+};
+
+document.getElementsByClassName("close")[0].onclick = function () {
+  document.getElementById("image-modal").style.display = "none";
+};
